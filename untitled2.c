280596ceb6b3d80d04079ae49d9c7f057ade26ae
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-10-15 20:46:19 +0530

    untitled2.c

--- untitled2.c
+++ untitled2.c
@@ -4,16 +4,16 @@ int main()
     int phy ,chem ,math ,avg;
 
     printf("\t\t\t***Student Eligibility calculator***\n\n\n");
-    printf("Enter Physics Marks");
-    scanf("%d,&phy");
-    printf("\nEnter Chemistry Marks");
-    scanf("%d,&chem");
-    printf("\nEnter Maths Marks");
-    scanf("%d,&math");
+    printf("Enter Physics Marks :");
+    scanf("%d",&phy);
+    printf("\nEnter Chemistry Marks :");
+    scanf("%d",&chem);
+    printf("\nEnter Maths Marks :");
+    scanf("%d",&math);
 
     avg=(phy+chem+math)/3;
 
-    printf("PCM Average = %d\n, avg");
+    printf("PCM Average = %d\n", avg);
     avg>=50?printf("You Are Eligible for admission"):printf("You Are Not Eligible For Adimission ");
 
     return 0;

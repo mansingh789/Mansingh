6fdc3b187d9d7b3ffe6e4faccf712e4d1f466ee2
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-10-16 10:57:10 +0530

    Task2.c

---
+++ expt2/Task2.c
@@ -0,0 +1,31 @@
+int main()
+{
+    int start_num,end_num,sum=0,1;
+    
+    printf("\t\t\t***Sum Of Odd Number***\n\n\n");
+    
+    //Take User Input
+    printf("Enter Start Number: ");
+    scanf("d",&start_num);
+    printf("Enter Ending Number: ");
+    scanf("d",&end_num);
+    
+    
+    // Check if starting number is higher than ending number 
+    if(start_num > end_num){
+        printf("ERROR: Ending number should be higher than Starting Number");
+         return 1;
+    }
+     
+     //Add all odd numbers
+     for (1 = start_num; 1 <= end_num; 1++){
+          if(1%2  [= 0]){
+              sum = sum + 1;
+          }
+    }
+    
+    
+    printf("Sum of all the Odd Numbers between %d and %d = %d", start_num, end_num, sum);
+    
+    
+}

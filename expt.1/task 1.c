34a0bf3712bfc49e6df999b7f423db167fd265db
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-12 14:21:58 +0530

    task 1.c

---
+++ expt.1/task 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{    
+        printf("Full Name\t: Mansingh Chauhan\n");
+        printf("Age\t: 17\n");
+        printf("Class\t: FE\n");
+        printf("Division\t: E\n");
+        printf("UIN\t: 241S018\n");
+        
+        return 0;
+}

a9811d6748a076f42a4f1474def919d3f823af3c
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 09:42:21 +0530

    Create Task 1.c

---
+++ Expt 4/Task 1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int isPrime(int num){
+if (num < 2){
+return 0;
+
+}
+int i;
+for (i = 2; i * i <= num; i++){
+if (num % i == 0)
+
+{
+return 0;
+
+}
+
+}
+return 1;
+}
+
+void findPrimes(int start, int end){
+
+printf("Prime numbers between %d and %d are:\n", start, end);
+int i;
+for ( i = start; i <= end; i++)
+{
+if (isPrime(i))
+
+{
+
+printf("%d ", i);
+}
+
+}
+printf("\n");
+}
+int main(){
+int start, end;
+
+printf("Enter the starting number: ");
+scanf("%d", &start);
+printf("Enter the ending number: ");
+scanf("%d", &end);
+findPrimes(start, end);
+return 0;
+}

c67b556c3d0319606cee24d52c6cf0951bf01e9e
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 09:53:06 +0530

    Create Task 1.c

---
+++ Expt 5/Task 1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int factorialRecursive(int n) {
+if (n == 0 || n == 1) {
+return 1;
+
+}
+return n * factorialRecursive(n - 1);
+
+}
+int factorialIterative(int n) {
+int factorial = 1;
+int i;
+    for ( i = 1; i <= n; i++) {
+
+factorial *= i;
+
+}
+
+   return factorial;
+}
+
+int main() {
+int num, result;
+     printf("\t\tFACTORIAL OF NUMBER\n");
+     printf("Enter a number to find its factorial: ");
+     scanf("%d", &num);
+
+     if (num < 0) {
+     printf("Factorial is not defined for negative numbers.\n");
+
+     } else {
+     result = factorialIterative(num);
+
+     printf("Factorial of %d using iterative function is: %d\n", num, result);
+     result = factorialRecursive(num);
+
+    printf("Factorial of %d using recursive function is: %d\n", num, result);
+}
+return 0;
+
+}

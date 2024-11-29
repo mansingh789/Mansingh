ed93ff74f9770ea6e89c7aa8eddf161bacb8ed23
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 13:25:50 +0530

    Update Task 2.c

--- Expt 8/Task 2.c
+++ Expt 8/Task 2.c
@@ -5,3 +5,46 @@
   Roll No    :-17
   UIN        :-241S018
   */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[100];
+    int length, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = 0;
+
+  while (str[length] != '\0') {
+  length++;
+ }
+
+   int i;
+    for ( i = 0; i < length / 2; i++) {
+    if (str[i] != str[length - i - 1]) {
+
+     isPalindrome = 0;
+   break;
+
+ }
+
+ }
+
+ if (isPalindrome) {
+     printf("The string is a palindrome.\n");
+
+     } else {
+
+     printf("The string is not a palindrome.\n");
+     }
+ return 0;
+
+}
+/* OUTPUT
+Enter a string: RADAR
+The string is a palindrome.
+/*
+

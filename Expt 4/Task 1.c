e1cab56cca92b4a27ead194729bf876ed82ca883
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 11:39:07 +0530

    Update Task 1.c

--- Expt 4/Task 1.c
+++ Expt 4/Task 1.c
@@ -1,6 +1,6 @@
 /*
-  Title      :- WAP to get students PCM marks from the user and find the average. Use conditional 
-              operator to print if the student is eligible for admission. Eligibility criteria is 50% in PCM.
+  Title      :- WAP to find all the prime numbers
+between two numbers.
   Name       :-Mansingh Chauhan
   Division.  :-F.E.(ECS) - E
   Roll No    :-17

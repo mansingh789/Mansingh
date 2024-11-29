008c20a72a9aafc728309f68302897318aa78c0a
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 10:32:53 +0530

    Update Task 1.c

--- Expt 8/Task 1.c
+++ Expt 8/Task 1.c
@@ -4,14 +4,15 @@ int main(){
 
     int len=0;
     char str[len];
-    printf("Enter Any String: ");
+    printf("\tFINDING STRING LENTH\n\n");
+    printf("Enter Your String: ");
     gets(str);
 
     while(str[len]!='\0'){
           len++;
     }
 
-    printf("Your String's Length is %d",len);
+    printf("Your String Length has %d Character(s)\n",len);
 
     return 0;
 

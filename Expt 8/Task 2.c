ea32c9d4ebb62b64dfdd4da345bfc081e0c9f71b
mansingh789 <mansinghch@eng.rizvi.edu.in>
2024-11-29 11:47:32 +0530

    Create Task 2.c

---
+++ Expt 8/Task 2.c
@@ -0,0 +1,7 @@
+/*
+  Title      :WAP to check if the entered string is palindrome or not.
+  Name       :-Mansingh Chauhan
+  Division.  :-F.E.(ECS) - E
+  Roll No    :-17
+  UIN        :-241S018
+  */
